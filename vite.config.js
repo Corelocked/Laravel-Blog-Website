3afcaa52a4f49ac3a1f377f9d5699eed34a595cd
Mateusz <mateusz.zaborski1@gmail.com>
2024-01-03 14:03:57 +0100

    fix: add missing JS file to the build process

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/admin.css', 'resources/js/post.js', 'resources/js/createPost.js', 'resources/js/filtr.js', 'resources/js/profile.js', 'resources/js/history.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/admin.css', 'resources/js/post.js', 'resources/js/createPost.js', 'resources/js/filtr.js', 'resources/js/profile.js', 'resources/js/history.js', 'resources/js/highlight.js'],
             refresh: true,
             server: {
                 host: 'localhost',
